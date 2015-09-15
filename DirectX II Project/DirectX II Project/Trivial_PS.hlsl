79bc364f4717e935d9bc0892b5447320fac69e2a
Anthony Melo <tuto.am@gmail.com>
2015-09-15 00:53:44 -0400

    Point light in point light and direction light combined

--- DirectX II Project/DirectX II Project/Trivial_PS.hlsl
+++ DirectX II Project/DirectX II Project/Trivial_PS.hlsl
@@ -18,25 +18,30 @@ SamplerState filter : register(s0);
 float4 main(P_IN modulate) : SV_TARGET
 {
 	//direction lighting
-	float3 glightDir = {1, 1, 1};
+	float3 Point_glightpos = { 0, 0, 0 };
+	float3 Direction_glightDir = { 1, 1, 1 };
 
-	//float3 ldir = -normalize(glightDir);
-	float3 ldir = -normalize(glightDir);
+	float3 Direction_ldir = -normalize(Direction_glightDir);
+	float3 Point_ldir = -normalize(Point_glightpos - modulate.projectedCoordinate);
 	float3 wnrm = normalize(modulate.norm);
 
-	float LightRatio = clamp(dot(-ldir, wnrm), 0, 1);
-	LightRatio *= 0.99f;
+	float Direction_LightRatio = clamp(dot(-Direction_ldir, wnrm), 0, 1);
+	Direction_LightRatio *= 0.99f;
+
+	float Point_LightRatio = clamp(dot(Point_ldir, modulate.norm), 0, 1);
 
 		//MultiTexturing
-	float4 baseColor = baseTexture.Sample(filter, modulate.UV.xy);
-	float4 newColor = SecondTexture.Sample(filter, modulate.UV.xy);
-	
-	float4 Final = baseColor * newColor;
-	
-	//Directional Light
-	float4 ambientColor = { 1, 1, 1, 1 };
-	
-	return (LightRatio * ambientColor * Final) + 0.01f;
+		float4 baseColor = baseTexture.Sample(filter, modulate.UV.xy);
+		float4 newColor = SecondTexture.Sample(filter, modulate.UV.xy);
+
+		float4 Final = baseColor * newColor;
+
+		//Directional Light
+		float4 Direction_ambientColor = { 1, 1, 1, 1 };
+		float4 Point_ambientColor = { 1, 1, 1, 1 };
+
+		//return (LightRatio * ambientColor * Final) + 0.01f;
+		return (Point_LightRatio * Point_ambientColor * Final) + ((Direction_LightRatio * Direction_ambientColor * Final) + 0.01f);
 
 	//return Final;
 }
